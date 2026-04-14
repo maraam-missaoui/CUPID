44ba26cd2c5433525ab00704109e3a382643dc63
maraam-missaoui <maraammissaoui@gmail.com>
2026-04-14 16:22:25 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 // ===== PRODUCT DATA =====
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  })
+    .then(() => navigate("/thank-you/"))
+    .catch(error => alert(error));
+};
 const PRODUCTS = [
   {
     id: 1,
